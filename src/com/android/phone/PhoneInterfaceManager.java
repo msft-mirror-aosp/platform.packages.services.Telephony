095be0bb467e9bde25726e9edf987c9a2bc0d00b
Thomas Stuart <tjstuart@google.com>
2024-06-06 22:34:33 +0000

    enforce the calling package is the default dialer for VisualVoicemail
    In the docs for setVisualVoicemailSmsFilterSettings, they state that
    the callingPackage needs to be the default dialer. However, server side,
    there is no enforcement for this. Now, every client is verified to be
    the default dialer, system dialer, or carrier visual voicemail app
    before changing the settings for visual voicemail.
    
    Bug: 308932906
    Test: CTS
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:60833ab6168a325da0e715b9116b3653a84b4c22)
    Merged-In: I951d7783f5c425c03768efb7aee7a38e299e6536
    Change-Id: I951d7783f5c425c03768efb7aee7a38e299e6536

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -4080,7 +4080,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
     public void enableVisualVoicemailSmsFilter(String callingPackage, int subId,
             VisualVoicemailSmsFilterSettings settings) {
         mAppOps.checkPackage(Binder.getCallingUid(), callingPackage);
-
+        enforceVisualVoicemailPackage(callingPackage, subId);
         final long identity = Binder.clearCallingIdentity();
         try {
             VisualVoicemailSmsFilterConfig.enableVisualVoicemailSmsFilter(
